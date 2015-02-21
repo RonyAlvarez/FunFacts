fb492ef6963516331bce93c678348738d0a30cd9
RonyAlvarez <ronyalvarez1993@gmail.com>
2015-02-21 12:40:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rons/funfacts/FunFactsActivity.java
@@ -0,0 +1,54 @@
+        package com.example.rons.funfacts;
+
+        import android.app.Activity;
+        import android.graphics.Color;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.RelativeLayout;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import java.util.Random;
+
+
+        public class FunFactsActivity extends Activity {
+
+            public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+            private FactBook mFact = new FactBook();
+            private ColorWheel mColorWheel = new ColorWheel();
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_fun_facts);
+
+                //declare our view variables and assign the views from the layout file
+                final TextView factLabel = (TextView) findViewById(R.id.FactTextView);
+                final Button showFactButton = (Button) findViewById(R.id.ShowFactButton);
+                final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+                View.OnClickListener listener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String fact = mFact.getFact();
+
+                        // update the label with out dynamic fact
+                        factLabel.setText(fact);
+
+                        int color = mColorWheel.getColor();
+                        relativeLayout.setBackgroundColor(color);
+
+                        showFactButton.setTextColor(color);
+                    }
+                };
+                showFactButton.setOnClickListener(listener);
+                //Toast.makeText(this, "Yay our activity was created!", Toast.LENGTH_LONG).show();
+                Log.d("FunFacts Activity", "We are logging from the onCreate() method!");
+
+    }
+}
